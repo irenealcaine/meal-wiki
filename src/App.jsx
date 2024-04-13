a02a19089d04c4d19082229bcd1488c156f111ab
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-13 16:54:27 +0200

    add layout and header components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from './Pages/Home/Home'
+import Layout from './Components/Layout/Layout';
 
 function App() {
 
   return (
     <div className='app'>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
       </BrowserRouter>
     </div>
   )
