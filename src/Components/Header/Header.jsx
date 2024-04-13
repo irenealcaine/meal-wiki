204ed07da5c63050a984f4bfd8071a4e28598c04
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-13 17:10:57 +0200

    update header styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import './Header.css'
 import React from 'react'
 
+import logo from '../../assets/images/logo512.png'
+
 const Header = () => {
+
+  const navItems = ['imgredients', 'zones', ' 3 things']
+
   return (
-    <header className='header'>Header</header>
+    <header className='header'>
+      <img src={logo} alt="logo" className='logo' />
+      <nav>
+        <ul>
+          {navItems.map((navItem) => (
+            <li key={navItem}>{navItem}</li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   )
 }
 
