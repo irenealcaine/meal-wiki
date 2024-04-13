770f8772298b4a9c4c5cacae30fcc1f3b41ed20b
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-13 16:48:18 +0200

    add react-router-dom and principal route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from './Pages/Home/Home'
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
