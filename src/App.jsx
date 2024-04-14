b9ad41bfc66a87beca71e23f95e3a8389dc2c436
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-14 18:11:11 +0200

    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from './Pages/Home/Home'
 import Layout from './Components/Layout/Layout';
+import NotFound from './Pages/NotFound/NotFound';
+import Categories from './Pages/Categories/Categories';
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </BrowserRouter>
