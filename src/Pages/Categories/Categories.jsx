acb6d38b4135473d394acea703a119cb16e978fa
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-14 18:21:03 +0200

    upgrade categories page

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { requests } from '../../Utils/constants';
 
 const Categories = () => {
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch(requests.mealCategories)
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data.categories);
+        console.log(categories)
+      });
+  }, [])
+
   return (
-    <div>Categories</div>
+    <div>
+      <h1>Categories</h1>
+      {categories.map((category) => (
+        <div key={category.idCategory}>
+          <h2>{category.strCategory}</h2>
+          <img src={category.strCategoryThumb} alt="" />
+          <p>{category.strCategoryDescription}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
