b490529139282cce070aed92cced91aa30a3fa87
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-16 16:36:29 +0200

    add link to details in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css'
 import React, { useEffect, useState } from 'react'
 import { requests } from '../../Utils/constants';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -21,8 +22,10 @@ const Home = () => {
       <h1>Home</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio quas asperiores quisquam perferendis? Magnam quae consectetur quia sint et deserunt qui. Distinctio ratione deleniti voluptate qui nesciunt dolor in soluta?</p>
       <h2>A random meal you may like</h2>
-      <p>{meal?.strMeal}</p>
-      <img src={meal?.strMealThumb} alt={meal?.strMeal} />
+      <Link to={`/${meal.idMeal}`}>
+        <p>{meal?.strMeal}</p>
+        <img src={meal?.strMealThumb} alt={meal?.strMeal} />
+      </Link>
     </div>
   )
 }
