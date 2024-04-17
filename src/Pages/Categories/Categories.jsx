5b09be2eee26f5e3ca8a07490218c4aefc20e0a5
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-17 16:30:07 +0200

    update Categories.jsx

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -20,6 +20,8 @@ const Categories = () => {
   return (
     <div>
       <h1>Categories</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, ducimus eveniet? Voluptas dignissimos inventore eligendi numquam, reiciendis aliquam ducimus soluta saepe itaque est. Eveniet quis dolorem ipsam consequuntur molestias officia!
+        Ex officia distinctio labore ipsam eum rerum necessitatibus animi ut molestiae veritatis eos aliquam nam dolorum nostrum, reiciendis aspernatur deleniti perspiciatis praesentium dignissimos culpa dolores ullam fuga commodi itaque? Ad.</p>
       <GridContainer>
         {categories.map((category) => (
           <Link key={category.idCategory} to={`/categories/${category.strCategory}`}>
