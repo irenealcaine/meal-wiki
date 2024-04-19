ec8168fc98299a818704db6571bed15f8c244b51
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-19 11:51:42 +0200

    update header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,35 @@
 import './Header.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 import logo from '../../assets/images/logo512.png'
 import { navItems } from '../../Utils/constants'
 import { Link } from 'react-router-dom'
 
+import { FaHamburger } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+
 const Header = () => {
 
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <header className='header'>
-      <Link to={'/'}><img src={logo} alt="logo" className='logo' /></Link>
-      <nav>
+      <Link to={'/'} className={`logo ${isOpen ? 'open' : ''}`}><img src={logo} alt="logo" className='logo' /></Link>
+      <nav className={`${isOpen ? 'open' : ''}`}>
         <ul>
+          <li>
+            <Link to={'/'} className={`home-link ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(false)}><img src={logo} alt="logo" className='logo' /></Link>
+          </li>
           {navItems.map((navItem) => (
             <li key={navItem.name}>
-              <Link to={navItem.to}>{navItem.name}</Link>
+              <Link to={navItem.to} onClick={() => setIsOpen(false)}>{navItem.name}</Link>
             </li>
           ))}
         </ul>
       </nav>
+      <div className={`open-nav-button`} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <IoClose /> : <FaHamburger />}
+      </div>
     </header>
   )
 }
