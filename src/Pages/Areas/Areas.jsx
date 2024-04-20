e0e66fd7a5d17a149e807223090cd8579c30746c
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-20 15:36:38 +0200

    add loaders and upgrade footer styles

--- src/Pages/Areas/Areas.jsx
+++ src/Pages/Areas/Areas.jsx
@@ -3,25 +3,34 @@ import { requests } from '../../Utils/constants';
 import { Link } from 'react-router-dom';
 import GridContainer from '../../Components/Layout/Grid/GridContainer';
 import GridItem from '../../Components/Layout/Grid/GridItem';
+import Loader from '../../Components/Loader/Loader';
 
 // https://www.flaticon.com/packs/countrys-flags <-- FLAGS ICONS
 
 const Areas = () => {
 
   const [areas, setAreas] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     fetch(requests.mealAreas)
       .then((res) => res.json())
       .then((data) => {
         setAreas(data.meals);
-        // console.log(areas)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
       });
   }, [])
 
   return (
     <div>
       <h1>Areas</h1>
+
+      {loading && <Loader />}
+
       <GridContainer columns={4}>
         {areas.map((area) => (
           <Link key={area.strArea} to={`/areas/${area.strArea}`}>
--- src/Pages/Ingredients/Ingredients.jsx
+++ src/Pages/Ingredients/Ingredients.jsx
@@ -3,23 +3,32 @@ import { requests } from '../../Utils/constants';
 import { Link } from 'react-router-dom';
 import GridContainer from '../../Components/Layout/Grid/GridContainer';
 import GridItem from '../../Components/Layout/Grid/GridItem';
+import Loader from '../../Components/Loader/Loader';
 
 const Ingredients = () => {
 
   const [ingredients, setIngredients] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     fetch(requests.mealIngredients)
       .then((res) => res.json())
       .then((data) => {
         setIngredients(data.meals);
-        // console.log(ingredients)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
       });
   }, [])
 
   return (
     <div>
       <h1>Ingredients</h1>
+
+      {loading && <Loader />}
+
       <GridContainer columns={4}>
         {ingredients.slice(0, 50).map((ingredient) => (
           <Link key={ingredient.idIngredient} to={`/ingredients/${ingredient.strIngredient}`}>
