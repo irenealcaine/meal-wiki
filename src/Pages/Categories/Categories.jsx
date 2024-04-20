ca2c29af6e1b2bd68c96731c3702ca634a04738b
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-20 15:29:52 +0200

    add loader

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -3,18 +3,25 @@ import { requests } from '../../Utils/constants';
 import { Link } from 'react-router-dom';
 import GridContainer from '../../Components/Layout/Grid/GridContainer';
 import GridItem from '../../Components/Layout/Grid/GridItem';
+import Loader from '../../Components/Loader/Loader';
 
 const Categories = () => {
 
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+
 
   useEffect(() => {
+    setLoading(true)
     fetch(requests.mealCategories)
       .then((res) => res.json())
       .then((data) => {
         setCategories(data.categories);
-        // console.log(categories)
-      });
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }, [])
 
   return (
@@ -22,6 +29,9 @@ const Categories = () => {
       <h1>Categories</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, ducimus eveniet? Voluptas dignissimos inventore eligendi numquam, reiciendis aliquam ducimus soluta saepe itaque est. Eveniet quis dolorem ipsam consequuntur molestias officia!
         Ex officia distinctio labore ipsam eum rerum necessitatibus animi ut molestiae veritatis eos aliquam nam dolorum nostrum, reiciendis aspernatur deleniti perspiciatis praesentium dignissimos culpa dolores ullam fuga commodi itaque? Ad.</p>
+
+      {loading && <Loader />}
+
       <GridContainer columns={4}>
         {categories.map((category) => (
           <Link key={category.idCategory} to={`/categories/${category.strCategory}`}>
