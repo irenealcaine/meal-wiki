86791b4df70dcf0a98b7f5e027ac56ef55f23f49
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-21 14:56:10 +0200

    add loader in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,17 +2,20 @@ import './Home.css'
 import React, { useEffect, useState } from 'react'
 import { requests } from '../../Utils/constants';
 import { Link } from 'react-router-dom';
+import Loader from '../../Components/Loader/Loader';
 
 const Home = () => {
 
   const [meal, setMeal] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     fetch(requests.randomMeal)
       .then((res) => res.json())
       .then((data) => {
         setMeal(data.meals[0]);
-        // console.log(meal)
+        setLoading(false)
       });
   }, [])
 
@@ -22,6 +25,7 @@ const Home = () => {
       <h1>Home</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio quas asperiores quisquam perferendis? Magnam quae consectetur quia sint et deserunt qui. Distinctio ratione deleniti voluptate qui nesciunt dolor in soluta?</p>
       <h2>A random meal you may like</h2>
+      {loading && <Loader />}
       <Link to={`/${meal.idMeal}`}>
         <h3>{meal?.strMeal}</h3>
         <img src={meal?.strMealThumb} alt={meal?.strMeal} />
