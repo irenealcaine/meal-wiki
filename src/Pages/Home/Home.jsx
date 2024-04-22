d5a90b1429ab30b5fb2f0a66178b88a72e3af6e0
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-22 19:18:32 +0200

    update Home.css and Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -73,11 +73,11 @@ const Home = () => {
       </Link>
       <h2>Search by first letter</h2>
       <p className='letters'>{letters.map((letter) => (
-        <div
+        <span
           key={letter}
           onClick={() => setSelectedLetter(letter)}
           className={`${selectedLetter === letter ? 'active' : ''}`}
-        >{letter}</div>
+        >{letter}</span>
       ))}</p>
       <GridContainer columns={4}>
         {
